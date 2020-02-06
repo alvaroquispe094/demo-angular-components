115d5531ad0600a650a4f9f012b2964760f9e4d8
Álvaro Daniel Quispe <aquispe@pl-jrhernandez.cx.ar>
2020-02-06 15:07:15 -0300

    dfix

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/test-components'));
+app.use(static(__dirname+'/dist/test-components'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/test-components/index.html'));
+    res.sendFile(join(__dirname+'/dist/test-components/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
